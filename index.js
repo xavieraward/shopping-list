87ff92fa491904589dd92a97ccacc5325862572b
Xavier Ward <xav.ward@gmail.com>
2019-04-07 11:23:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function addToList() {
+$(function() {
     ('#shopping-list-entry').on('submit', function(e) {
     var text = $('#shopping-list-entry').val();
-    ('#shopping-item').append('<span>' + text + '</span>');
+    ('#shopping-item').append(`<li>
+    <span class="shopping-item">${text}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
     e.preventDefault();
 }
 
-)};
+)});
 
 function removeItem() {
 
